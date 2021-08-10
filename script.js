ae02ddfe0eef3eb3b5c2455055f86dcc8ba74bbc
Justin <justin.ng147@gmail.com>
2021-08-10 00:21:30 -0700

    added playRound and fixed syntax

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 function computerPlay(){
     switch(Math.floor(Math.random()*3)){
         case 0:
-            return 'Rock'
+            return 'Rock';
         case 1:
-            return 'Paper'
+            return 'Paper';
         case 2:
-            return 'Scissors'
+            return 'Scissors';
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection= playerSelection.toLowerCase();
+    computerSelection= computerSelection.toLowerCase();
+    let loseMessage = `You Lose!\n${playerSelection} loses to ${computerSelection}`;
+    let winMessage = `You Win!\n${playerSelection} beats ${computerSelection}`;
 
-console.log(computerPlay())
+    if(playerSelection == computerSelection){
+        // console.log('test2')
+        return "You Tie!\nGo Again";
+    }
+
+    if(playerSelection=='rock'){
+        // console.log('test')
+        // console.log('bla'+computerSelection)
+        if(computerSelection=='paper'){
+            return loseMessage;
+        } 
+        else if(computerSelection=='scissors'){
+            return winMessage;
+        }
+    }
+    else if(playerSelection=='paper'){
+        if(computerSelection=='scissors'){
+            return loseMessage;
+        } 
+        else if(computerSelection=='rock'){
+            return winMessage;
+        }
+    }
+    else if(playerSelection=='scissors'){
+        if(computerSelection=='rock'){ 
+            return loseMessage;
+        }
+        else if(computerSelection=='paper'){
+            return winMessage;
+        }
+    }
+}
+
+console.log('start');
+console.log(computerPlay());
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
